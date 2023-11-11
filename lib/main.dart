2fd2176b4d26abfb8aa6f43374ab5f1125df8c2c
Mateus Marinho <mateus.marinho@ufrpe.br>
2023-11-11 20:53:44 -0300

    Added firebase config to config folder

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:behome/services/auth_service.dart';
 import 'package:behome/widgets/auth_check.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'config/firebase_options.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
