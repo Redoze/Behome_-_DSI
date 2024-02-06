cd95c7298592a6684e917d94b5cf26950077a9d8
Markie98 <40475028+Markie98@users.noreply.github.com>
2024-02-06 17:43:23 -0300

    Serviço de Ambientes
    Adicionado serviço de ambientes para o Firestore.

---
+++ lib/services/environment_service.dart
@@ -0,0 +1,35 @@
+import 'package:behome/models/environment_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class EnvironmentService {
+  final CollectionReference _environmentsCollection =
+      FirebaseFirestore.instance.collection('environments');
+
+  Stream<List<EnvironmentModel>> readCategories(String homeId) {
+    return _environmentsCollection
+        .where('homeId', isEqualTo: homeId)
+        .where('isActive', isEqualTo: true)
+        .snapshots()
+        .map((snapshot) => snapshot.docs
+            .map((doc) => EnvironmentModel.fromFirestore(
+                doc.data() as Map<String, dynamic>, doc.id))
+            .toList())
+        .handleError((error) {
+      return <EnvironmentModel>[];
+    });
+  }
+
+  Future<void> createEnvironment(EnvironmentModel environment) async {
+    DocumentReference docRef = _environmentsCollection.doc();
+    environment.id = docRef.id;
+    await docRef.set(environment.toJson());
+  }
+
+  Future<void> updateEnvironment(EnvironmentModel environment) async {
+    await _environmentsCollection.doc(environment.id).update(environment.toJson());
+  }
+
+  Future<void> deleteEnvironment(String id) async {
+    await _environmentsCollection.doc(id).update({'isActive': false});
+  }
+}
