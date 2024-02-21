9806db0f6998aee43d2c42bdc37b071507ea5b67
José Fernando de Oliveira Filho <57115855+fernandooliveira7@users.noreply.github.com>
2024-02-20 21:49:46 -0300

    Update settings_page.dart

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:behome/pages/categories_page.dart';
+import 'package:behome/pages/residentes_page.dart';
 import 'package:behome/pages/login_page.dart';
 import 'package:provider/provider.dart';
 import '../services/auth_service.dart';
 
 class SettingsPage extends StatefulWidget {
-  const SettingsPage({super.key});
+  const SettingsPage({Key? key});
 
   @override
   State<SettingsPage> createState() => _SettingsPageState();
@@ -17,228 +18,190 @@ class _SettingsPageState extends State<SettingsPage> {
     return Scaffold(
       appBar: AppBar(),
       body: Center(
-          child: Container(
-        alignment: Alignment.topLeft,
-        child: Column(
-          children: [
-            const Padding(
-              padding: EdgeInsets.all(20),
-              child: Column(
-                children: [
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Configurações",
-                        style: TextStyle(
-                          fontSize: 32,
-                          fontWeight: FontWeight.bold,
+        child: Container(
+          alignment: Alignment.topLeft,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Configurações",
+                          style: TextStyle(
+                            fontSize: 32,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(height: MediaQuery.of(context).size.height * 0.1),
-            Padding(
-              padding: EdgeInsets.all(20),
-              child: Column(
-                children: [
-                  const Row(
-                    children: [
-                      IconButton(
-                        icon: Icon(Icons.person_add),
-                        color: Color.fromRGBO(8, 85, 255, 1),
-                        onPressed: null,
-                      ),
-                      Flexible(
-                        fit: FlexFit.tight,
-                        child: Padding(
-                          padding: EdgeInsets.all(12.0),
-                          child: Column(
-                            children: [
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    "Adicionar Pessoa",
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    "Adicine uma nova pessoa ao seu BeHome.",
-                                    style: TextStyle(
-                                      fontSize: 10,
-                                      fontWeight: FontWeight.w300,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 20),
-                  Row(
-                    children: [
-                      IconButton(
-                        icon: const Icon(Icons.add_home_work_rounded),
-                        color: const Color.fromRGBO(255, 127, 8, 1),
-                        onPressed: () {
-                          Navigator.push(
+              SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.person_add),
+                          color: Color.fromRGBO(8, 85, 255, 1),
+                          onPressed: () {
+                            Navigator.push(
                               context,
-                              MaterialPageRoute(
-                                  builder: (builder) => const CategoryPage()));
-                        },
-                      ),
-                      const Flexible(
-                        fit: FlexFit.tight,
-                        child: Padding(
-                          padding: EdgeInsets.all(12.0),
-                          child: Column(
-                            children: [
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    "Adicionar Categoria",
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
+                              MaterialPageRoute(builder: (builder) => AddPersonPage()),
+                            );
+                          },
+                        ),
+                        Flexible(
+                          fit: FlexFit.tight,
+                          child: Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: Column(
+                              children: [
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "Adicionar Pessoa",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
                                     ),
-                                  ),
-                                ],
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    "Adicine uma nova categoria de despesas.",
-                                    style: TextStyle(
-                                      fontSize: 10,
-                                      fontWeight: FontWeight.w300,
+                                  ],
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "Adicione uma nova pessoa ao seu BeHome.",
+                                      style: TextStyle(
+                                        fontSize: 10,
+                                        fontWeight: FontWeight.w300,
+                                      ),
                                     ),
-                                  ),
-                                ],
-                              ),
-                            ],
+                                  ],
+                                ),
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                  /*               const SizedBox(height: 20),
-                  Row(
-                    children: [
-                      IconButton(
-                        icon: const Icon(Icons.add_business_rounded),
-                        color: const Color.fromRGBO(105, 255, 195, 1),
-                        onPressed: () {
-                          Navigator.push(
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.add_home_work_rounded),
+                          color: const Color.fromRGBO(255, 127, 8, 1),
+                          onPressed: () {
+                            Navigator.push(
                               context,
-                              MaterialPageRoute(
-                                  builder: (builder) => const EnvironmentPage()));
-                        },
-                      ),
-                      const Flexible(
-                        fit: FlexFit.tight,
-                        child: Padding(
-                          padding: EdgeInsets.all(12.0),
-                          child: Column(
-                            children: [
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    "Adicionar Ambiente",
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
+                              MaterialPageRoute(builder: (builder) => const CategoryPage()),
+                            );
+                          },
+                        ),
+                        Flexible(
+                          fit: FlexFit.tight,
+                          child: Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: Column(
+                              children: [
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "Adicionar Categoria",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
                                     ),
-                                  ),
-                                ],
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    "Adicione um novo ambiente de despesas.",
-                                    style: TextStyle(
-                                      fontSize: 10,
-                                      fontWeight: FontWeight.w300,
+                                  ],
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "Adicione uma nova categoria de despesas.",
+                                      style: TextStyle(
+                                        fontSize: 10,
+                                        fontWeight: FontWeight.w300,
+                                      ),
                                     ),
-                                  ),
-                                ],
-                              ),
-                            ],
+                                  ],
+                                ),
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),*/
-                  const SizedBox(height: 20),
-                  Row(
-                    children: [
-                      IconButton(
-                        icon: const Icon(Icons.logout),
-                        color: Colors.red,
-                        onPressed: () async {
-                          await Provider.of<AuthService>(context, listen: false)
-                              .logout();
+                      ],
+                    ),
+                    /* Outros botões removidos para simplificação */
+                    const SizedBox(height: 20),
+                    Row(
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.logout),
+                          color: Colors.red,
+                          onPressed: () async {
+                            await Provider.of<AuthService>(context, listen: false)
+                                .logout();
 
-                          Navigator.of(context).pushReplacement(
+                            Navigator.of(context).pushReplacement(
                               MaterialPageRoute(
-                                  builder: (context) => LoginPage()));
-                        },
-                      ),
-                      const Flexible(
-                        fit: FlexFit.tight,
-                        child: Padding(
-                          padding: EdgeInsets.all(12.0),
-                          child: Column(
-                            children: [
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    "Sair",
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ],
+                                builder: (context) => LoginPage(),
                               ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    "Deslogar da sua conta.",
-                                    style: TextStyle(
-                                      fontSize: 10,
-                                      fontWeight: FontWeight.w300,
+                            );
+                          },
+                        ),
+                        Flexible(
+                          fit: FlexFit.tight,
+                          child: Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: Column(
+                              children: [
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "Sair",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
                                     ),
-                                  ),
-                                ],
-                              ),
-                            ],
+                                  ],
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "Deslogar da sua conta.",
+                                      style: TextStyle(
+                                        fontSize: 10,
+                                        fontWeight: FontWeight.w300,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            )
-          ],
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
+
